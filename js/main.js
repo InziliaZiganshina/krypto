6406ee04b846b62179a2948a3edc6be940115f68
Inzilia Ziganshina <inzilia53@gmail.com>
2023-10-27 21:32:32 +0300

    cursor pointer styles

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const followCursor = () => { // объявление функции
+      const el = document.querySelector('.follow-cursor') // поиск элемента, который будет следовать за курсором
+  
+      window.addEventListener('mousemove', e => { // при движении курсора
+        const target = e.target // определяется, где находится курсор
+        if (!target) return
+  
+        if (target.closest('a')) { // если курсор наведён на ссылку
+          el.classList.add('follow-cursor_active') // элементу добавляется активный класс
+        } else { // иначе
+          el.classList.remove('follow-cursor_active') // удаляется активный класс
+        }
+  
+        el.style.left = e.pageX + 'px' //элементу задается позиционирование слева
+        el.style.top = e.pageY + 'px' //элементу задается позиционирование сверху
+      })
+    }
+  
+    followCursor() // вызываем функцию followCursor
+  
+  })
+  
