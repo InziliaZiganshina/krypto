9d5b2d2f6b130e65b143ea5c8dc65f4f17dcb9db
Inzilia Ziganshina <inzilia53@gmail.com>
2023-11-07 02:14:12 +0300

    return

--- js/main.js
+++ js/main.js
@@ -28,4 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
   menuBtn.addEventListener('click', ()=> {
     menuMobile.classList.toggle('menu--open');
   });
-  
+
+
+
+// let menuBtn = document.querySelector('.menu-btn');
+// let menu = document.querySelector('.menu');
+// menuBtn.addEventListener('click', function(){
+// 	menuBtn.classList.toggle('active');
+// 	menu.classList.toggle('active');
+// })
