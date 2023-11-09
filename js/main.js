742bff524f7f8eb34374a94c7dff88536da3cb7a
Inzilia Ziganshina <inzilia53@gmail.com>
2023-11-09 23:41:59 +0300

    @media 360px

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
 // let menuBtn = document.querySelector('.menu-btn');
 // let menu = document.querySelector('.menu');
 // menuBtn.addEventListener('click', function(){
